9a3fb0440ee35e3194bf15186ea756059ed64c4e
aarnadlr <aaronadler@gmail.com>
2020-08-09 13:02:10 -0400

    Set up form inputs.

--- src/components/CreditCardForm/CreditCardForm.tsx
+++ src/components/CreditCardForm/CreditCardForm.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import logos from '../../images/cardlogos.png'
-interface CreditCardFormProps {}
+import logos from '../../images/cardlogos.png';
+import { useForm } from "react-hook-form";
 
-export const CreditCardForm: React.FC<CreditCardFormProps> = ({ }) => {
+type Inputs = {
+  name: string,
+  cardNumber: number,
+  cvv2: number,
+  expMonth: number,
+  expYear: number,
+};
+
+const errorMsg = <span className='error-msg--required'>This field is required</span>;
+
+export const CreditCardForm: React.FC = () => {
   
-  const handleSubmit = () => {
-    
-  };
+  const { register, handleSubmit, errors } = useForm<Inputs>();
+  const onSubmit = (data:Inputs) => console.log(data);
 
   return (
-    <form className="CreditCardForm" onSubmit={handleSubmit}>
+    <form className="CreditCardForm" onSubmit={handleSubmit(onSubmit)}>
 
       <p>Please enter your card information</p>
 
-      <input type="text" name="name" id="name" />
+      <input ref={register({ required: true })} placeholder="Name" type="text" name="name" id="name" />
+      {errors.name && errorMsg}
 
-      <input type="number" name="card-number" id="card-number" />
-      <input type="number" name="cvv2" id="cvv2" />
+      <input ref={register({ required: true })} placeholder="Card number" type="text" name="cardNumber" id="cardNumber" />
+      {errors.cardNumber && errorMsg}
 
-      <input type="number" name="exp-month" id="exp-month" />
-      <input type="number" name="exp-year" id="exp-year" />
+      <input ref={register({ required: true })} placeholder="CVV2" type="number" name="cvv2" id="cvv2" />
+      {errors.cvv2 && errorMsg}
+
+      <input ref={register({ required: true })} placeholder="Exp Month" type="number" name="expMonth" id="expMonth" />
+      {errors.expMonth && errorMsg}
+      
+      <input ref={register({ required: true })} placeholder="Exp Year" type="number" name="expYear" id="expYear" />
+      {errors.expYear && errorMsg}
 
       <div className="image-wrapper">
         <img src={logos} alt=""/>
